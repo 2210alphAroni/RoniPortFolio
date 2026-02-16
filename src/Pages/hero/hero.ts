fe3076e903fb0e7aaa7f513e122f6b5ba253fe01
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-16 19:10:18 +0600

    building hero section....

--- src/Pages/hero/hero.ts
+++ src/Pages/hero/hero.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {
+export class Hero implements OnInit {
 
+  roles: string[] = [
+    'Full Stack Developer',
+    'Django & DRF Developer',
+    'Angular Developer',
+    'Dotnet Developer',
+  ];
+
+  currentRole: string = '';
+  roleIndex: number = 0;
+
+  ngOnInit(): void {
+    this.currentRole = this.roles[0];
+    setInterval(() => {
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      this.currentRole = this.roles[this.roleIndex];
+    }, 1000);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Hero } from '../Pages/hero/hero';
+import { About } from '../Pages/about/about';
+import { Contact } from '../Pages/contact/contact';
+import { Projects } from '../Pages/projects/projects';
+import { Resume } from '../Pages/resume/resume';
+import { Skills } from '../Pages/skills/skills';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Hero,
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'projects',
+    component: Projects
+  },
+  {
+    path: 'skills',
+    component: Skills
+  },
+  {
+    path: 'resume',
+    component: Resume
+  },
+  {
+    path: 'contact',
+    component: Contact
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
