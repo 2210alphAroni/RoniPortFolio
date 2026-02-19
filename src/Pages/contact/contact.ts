18180aac9b8b0cbfb19cf82dae9c6166ce95c7b4
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-19 16:26:50 +0600

    Contact page build done

--- src/Pages/contact/contact.ts
+++ src/Pages/contact/contact.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
 
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  onSubmit() {
+    console.log(this.contactData);
+    alert("Message Sent Successfully 🚀");
+    this.contactData = { name: '', email: '', message: '' };
+  }
 }
