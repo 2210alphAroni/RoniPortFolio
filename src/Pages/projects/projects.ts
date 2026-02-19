7d767ebf2ab0f0d50ee057bb767a28202edf23fa
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-19 17:08:13 +0600

    project page build done

--- src/Pages/projects/projects.ts
+++ src/Pages/projects/projects.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects {
+
+  projects = [
+    {
+      title: 'Photography Service Booking System',
+      tech: 'ASP.NET Core, Web API, Dapper',
+      description: 'Online booking system with payment integration.',
+      image: '/assets/img/Application_logo.png',
+      github: 'https://github.com/2210alphAroni/PSBSProject',
+      live: 'https://github.com/2210alphAroni/PSBSProject'
+    },
+    {
+      title: 'My Portfolio',
+      tech: 'HTML, CSS, JavaScript',
+      description: 'Modern animated portfolio with 3D skills cube.',
+      image: '/assets/img/favicon-img.png',
+      github: 'https://github.com/2210alphAroni/Portfolio',
+      live: 'https://my-portfolio-7kuk.onrender.com/'
+    }
+  ];
 
 }
