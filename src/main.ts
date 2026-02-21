fc06be414670f38b88ec3bf43b36b3c152ec3979
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-21 13:11:06 +0600

    update to apply rain effect in portfolio

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,23 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+  // For rain effect
+function createRain() {
+  const rainContainer = document.querySelector('.rain');
+  const numberOfDrops = 150;
+
+  for (let i = 0; i < numberOfDrops; i++) {
+    const drop = document.createElement('div');
+    drop.classList.add('drop');
+
+    drop.style.left = Math.random() * window.innerWidth + 'px';
+    drop.style.animationDuration = 0.5 + Math.random() * 0.5 + 's';
+    drop.style.animationDelay = Math.random() * 2 + 's';
+
+    rainContainer?.appendChild(drop);
+  }
+}
+
+window.onload = createRain;
