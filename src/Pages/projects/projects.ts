dbbc3f45b02aae05e3aac0ec9183a61ec4452322
Md Nabinur Islam Roni <22103371@iubat.edu>
2026-03-10 18:57:19 +0600

    add one more project

--- src/Pages/projects/projects.ts
+++ src/Pages/projects/projects.ts
@@ -10,15 +10,23 @@ import { RouterModule } from '@angular/router';
   styleUrl: './projects.css',
 })
 export class Projects {
-
   projects = [
+    {
+      title: 'MovBD - Movie Platform',
+      tech: 'React, Node.js, Express, MongoDB Atlas',
+      description:
+        "Bangladesh's premier movie platform with browsing, downloading, trailer watching, reviews & admin dashboard.",
+      image: '/assets/img/movbd.png',
+      github: 'https://github.com/2210alphAroni/movbd',
+      live: 'https://movbd.vercel.app',
+    },
     {
       title: 'Photography Service Booking System',
       tech: 'ASP.NET Core, Web API, Dapper',
       description: 'Online booking system with payment integration.',
       image: '/assets/img/Application_logo.png',
       github: 'https://github.com/2210alphAroni/PSBSProject',
-      live: 'https://github.com/2210alphAroni/PSBSProject'
+      live: 'https://github.com/2210alphAroni/PSBSProject',
     },
     {
       title: 'My Portfolio',
@@ -26,7 +34,7 @@ export class Projects {
       description: 'Modern animated portfolio with 3D skills cube.',
       image: '/assets/img/favicon-img.png',
       github: 'https://github.com/2210alphAroni/Portfolio',
-      live: 'https://my-portfolio-7kuk.onrender.com/'
+      live: 'https://my-portfolio-7kuk.onrender.com/',
     },
     {
       title: 'Device Zone E-commerce Platform',
@@ -34,26 +42,25 @@ export class Projects {
       description: 'E-commerce platform for electronics with user authentication.',
       image: '/assets/img/aboutpagephoto-removebg-preview.png',
       github: 'https://github.com/2210alphAroni/Device-Zone-E-Commerce-Website',
-      live: 'https://github.com/2210alphAroni/Device-Zone-E-Commerce-Website'
+      live: 'https://github.com/2210alphAroni/Device-Zone-E-Commerce-Website',
     },
     {
-      title: "Ramadan Tracker – Real-Time Islamic Productivity Web App",
-      tech: ["Next.js", "Tailwind CSS", "JavaScript", "REST API"],
+      title: 'Ramadan Tracker – Real-Time Islamic Productivity Web App',
+      tech: ['Next.js', 'Tailwind CSS', 'JavaScript', 'REST API'],
       description:
-        "A real-time Ramadan productivity tracking application that helps users monitor fasting, prayer, Quran reading, and daily spiritual goals with live Sehri and Iftar countdown.",
-      image: "/assets/img/Ramadantrackerapp.PNG",
-      github: "https://github.com/2210alphAroni/Ramadan-App",
-      live: "https://ramadantracker-wcn0.onrender.com/"
+        'A real-time Ramadan productivity tracking application that helps users monitor fasting, prayer, Quran reading, and daily spiritual goals with live Sehri and Iftar countdown.',
+      image: '/assets/img/Ramadantrackerapp.PNG',
+      github: 'https://github.com/2210alphAroni/Ramadan-App',
+      live: 'https://ramadantracker-wcn0.onrender.com/',
     },
     {
-      title: "Solar Harvest – Smart Farmer Web Application",
-      tech: ["Next.js", "Tailwind CSS", "JavaScript"],
+      title: 'Solar Harvest – Smart Farmer Web Application',
+      tech: ['Next.js', 'Tailwind CSS', 'JavaScript'],
       description:
-        "A modern agriculture-focused web application designed to support farmers with smart farming insights, solar-powered solutions awareness, and responsive dashboard interface for sustainable crop management.",
-      image: "/assets/img/favicon.png",
-      github: "https://github.com/2210alphAroni/Farmer-App",
-      live: "https://solarharvest.onrender.com/"
-    }
+        'A modern agriculture-focused web application designed to support farmers with smart farming insights, solar-powered solutions awareness, and responsive dashboard interface for sustainable crop management.',
+      image: '/assets/img/favicon.png',
+      github: 'https://github.com/2210alphAroni/Farmer-App',
+      live: 'https://solarharvest.onrender.com/',
+    },
   ];
-
 }
